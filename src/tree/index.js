844a1cafe68a420677f8ce6e53790b22e1bdaa3e
ZITANIMohamed98 <zitaniceo@gmail.com>
2023-08-21 20:53:49 +0200

    Second Part: data from json file

--- src/tree/index.js
+++ src/tree/index.js
@@ -1,30 +1,49 @@
 import React, {useState, useEffect} from 'react';
 import './index.css'
 import axios from 'axios'
+import data from './data.json'
+
+
+const TreeContent = () => {
+    const [currData, setCurrData] = useState(data);
+    
+    return (
+        <ul className="main">
+            
+            {!currData.items.length==0 && currData.items.map((item, i) => (
+                <React.Fragment key={i}>
+                    <li>{(i + 1) + ". " + item.name}</li>
+                        <ul>
+                        {item.subitems.map((subitem, j) => (
+                            <React.Fragment key={j}>
+                                <div className='node'><li >{(i + 1) + "." + (j + 1) + ". " + subitem.name}</li></div>
+                                
+                                <ul>
+                                    {subitem.subitems.map((subsubitem, k) => (
+                                        <React.Fragment key={k}>
+                                            <div className='node'><li >{(i + 1) + "." + (j + 1) + "." + (k + 1) + ". " + subsubitem.name}</li></div>
+                                            
+                                            <ul>
+                                                {subsubitem.subitems.map((subsubsubitem, h) => (
+                                                   <div className='node'> <li key={h} >{(i + 1) + "." + (j + 1) + "." + (k + 1) + "." + (h + 1) + '. ' + subsubsubitem.name}</li></div>
+                                                ))}
+                                            </ul>
+                                        </React.Fragment>
+                                    ))}
+                                </ul>
+                            </React.Fragment>
+                        ))}
+                    </ul>
+                </React.Fragment>
+            ))}
+        </ul>
+    );
+};
 
 const Tree = () => {
     return (
             <div className="tree">
-
-                <ul className='main'>
-                    <li>1. root</li>
-                    <ul className='node'>
-                            <li>1.1. ant</li>
-                    </ul>
-                    <ul className='node'>
-                            <li>1.2. bear</li>
-                            <ul className='node'>
-                                    <li>1.2.1. cat</li>
-                                    <li>1.2.2. dog</li>
-                                    <ul className='node'>
-                                        <li>1.2.2.1. elephant</li>
-                                    </ul>
-                            </ul>
-                    </ul>
-                    <ul className='node'>
-                            <li>1.3. frog</li>
-                    </ul>
-                </ul>
+                <TreeContent />
             </div>
     )
 }
